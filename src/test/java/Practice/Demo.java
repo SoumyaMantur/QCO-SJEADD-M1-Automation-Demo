e14660f48c8985eafc2963d7428a00a3b8396309
Soumya Mantur <soumyamantur123@gmail.com>
2023-05-01 11:03:47 +0530

    My First Commit

--- src/test/java/Practice/Demo.java
+++ src/test/java/Practice/Demo.java
@@ -1,18 +1,13 @@
 package Practice;
 
 public class Demo {
-	
+
 	public static void main(String[] args) {
 		
 		System.out.println("Hi This is My First Commit");
 		
-		System.out.println("Hi");
-		
-<<<<<<< HEAD
-		System.out.println("Bye");
-=======
-		System.out.println("Hello");
->>>>>>> branch 'master' of https://github.com/SoumyaMantur/QCO-SJEADD-M1-Automation-Demo.git
+		System.out.println("HI");
+
 	}
 
 }
